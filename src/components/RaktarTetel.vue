781028ae8142a4baf87759aed0d2e41092f38e97
Takacs Balazs <tbalazs.levente@gmail.com>
2021-12-07 15:02:46 +0100

    Kiiras

---
+++ src/components/RaktarTetel.vue
@@ -0,0 +1,33 @@
+<template>
+  <table>
+      <tr>
+          <th>{{ title }}</th>
+      </tr>
+      <tr>
+          <td>{{ price }}</td>
+      </tr>
+      <tr>
+          <td>{{ quantity }}</td>
+      </tr>
+      <tr>
+          <td>
+              <input type="text">
+              <input type="number">
+              <input type="number">
+          </td>
+      </tr>
+  </table>
+</template>
+
+<script>
+export default {
+    props: ['tetel'],
+    data() {
+        return {
+            title: this.tetel.title,
+            price: this.tetel.price,
+            quantity: this.tetel.quantity
+        }
+    }
+}
+</script>
